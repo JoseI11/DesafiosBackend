9f1b469e94b99553d068ca7af08cf48a8e2cb736
nacho <jose-imhoff@hotmail.com>
2023-02-27 17:53:16 -0300

    Desafio 2 cambios 4

--- desafios/desafios.js
+++ desafios/desafios.js
@@ -8,55 +8,61 @@ export default class ProductManager {
         if (fs.existsSync(this.path)) {
             const data = await fs.promises.readFile(this.path, 'utf-8');
             const result = JSON.parse(data);
-            console.log(result);
             return result;
         } else {
             return [];
         }
     }
     addProduct = async (code, title, description, price, thumbnail, stock) => {
-
-        if (!code || !title || !description || !price || !thumbnail || !stock) {
-            console.log("All the fields must be completed")
-            return;
-        }
-
-        this.products.forEach(element => {
-            if (element.code === code) {
-                console.log(`The field code ${code} is repeated`);
+        try {
+            if (!code || !title || !description || !price || !thumbnail || !stock) {
+                console.log("All the fields must be completed")
                 return;
             }
-        });
-
-        const product = {
-            code: code,
-            title: title,
-            description: description,
-            price: price,
-            thumbnail: thumbnail,
-            stock: stock,
-            id: this.products.length + 1
+    
+            let productRepeated = this.products.find((element) => element.code === code);
+            if (productRepeated) {
+                return `The field code ${code} is repeated so this product cannot be save in the list`;
+                
+            }
+            const product = {
+                code: code,
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                stock: stock,
+                id: this.products.length + 1
+            }
+    
+    
+            this.products.push(product);
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"));
+            return this.products
+        } catch (error) {
+            console.log(error);
         }
-
-
-        this.products.push(product);
-        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"));
-        return this.products
+     
     }
 
     getProductById = async (id) => {
         if (fs.existsSync(this.path)) {
-            const data = await fs.promises.readFile(this.path, 'utf-8');
-            const result = JSON.parse(data);
+            try {
+                const data = await fs.promises.readFile(this.path, 'utf-8');
+                const result = JSON.parse(data);
 
-            let indexValue = result.find((event) => event.id === id);
-            if (!indexValue) {
-                console.log("Not found");
-                return;
-            } else {
-             
-                return indexValue;
+                let indexValue = result.find((event) => event.id === id);
+                if (!indexValue) {
+                   
+                    return "This product  with this ID does not exist in the file";
+                } else {
+
+                    return indexValue;
+                }
+            } catch (error) {
+                console.log(error);
             }
+
         }
 
     }
@@ -66,12 +72,12 @@ export default class ProductManager {
             if (productFounded) {
                 try {
                     const valor = this.products.filter((event) => event.id != id);
-                 
+
                     this.products = valor;
-              
+
                     await fs.promises.writeFile(this.path, JSON.stringify(valor, null, "\t"))
                     return "Product eliminated";
-               
+
                 } catch (error) {
                     console.log(error);
                 }
@@ -84,13 +90,13 @@ export default class ProductManager {
 
     }
     updateProduct = async (id, code, title, description, price, thumbnail, stock) => {
-        let productExists=this.products.find((product)=>product.id ===id)
-        if(productExists){
+        let productExists = this.products.find((product) => product.id === id)
+        if (productExists) {
             try {
                 const productoAmodificar = this.products.filter((product) => product.id === id);
-      
+
                 const prod = {
-                
+
                     code: code ?? productoAmodificar[0].code,
                     title: title ?? productoAmodificar[0].title,
                     description: description ?? productoAmodificar[0].description,
@@ -99,21 +105,21 @@ export default class ProductManager {
                     stock: stock ?? productoAmodificar[0].stock,
                     id: id
                 }
-               
+
                 this.products[id - 1] = prod;
-        
+
                 //console.log(this.products)
                 await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"));
                 return "Product updated";
             } catch (error) {
                 console.log(error)
             }
-           
-        }else{
+
+        } else {
             return `The product to update with the id ${id} does not exist in the list`;
         }
 
-     
+
     }
 }
 
--- desafios/index.js
+++ desafios/index.js
@@ -1,18 +1,24 @@
 import ProductManager from './desafios.js';
 const manager = new ProductManager();
 const operacionesProductos = async () => {
+    try {
+        let product1 = await manager.addProduct("PR01", "Mouse Inalambrico Logitech", "Negro con led roja", 200, "mouse.jpg", 50)
+        console.log(product1)
+        let product2 = await manager.addProduct("PR01", "Teclado Inalambrico Logitech", "Blanco", 100, "teclado.jpg", 25)
+        console.log(product2)
 
-    let product1 = await manager.addProduct("PR01", "Mouse Inalambrico Logitech", "Negro con led roja", 200, "mouse.jpg", 50)
-    console.log(product1)
-    let product2 = await manager.addProduct("PR02", "Teclado Inalambrico Logitech", "Blanco", 100, "teclado.jpg", 25)
-    console.log(product2)
+         let segundaConsulta = await manager.getProducts();
+         console.log(segundaConsulta);
+         let productoId= await manager.getProductById(2)
+         console.log(productoId);
+        let productAct = await manager.updateProduct(2, "Mouse");
+        console.log(productAct);
+        let deleteproduct1 = await manager.deleteProducts(3);
+        console.log(deleteproduct1)
+    } catch (error) {
+        console.log(error);
+    }
 
-    let segundaConsulta = await manager.getProducts();
-    console.log(segundaConsulta);
-    let productAct = await manager.updateProduct(2, "Mouse");
-    console.log(productAct);
-    let deleteproduct1 = await manager.deleteProducts(3);
-    console.log(deleteproduct1)
 
 }
 operacionesProductos();
