639459014b4cbbce58c278500321d2ba3f499ba3
nacho <jose-imhoff@hotmail.com>
2023-03-06 18:09:52 -0300

    Desafio 3 Cambio 3

--- desafios/src/app.js
+++ desafios/src/app.js
@@ -2,46 +2,52 @@ import ProductManager from './ProductManager.js';
 import express from "express";
 const manager = new ProductManager();
 const productServer = express();
-const operacionesProductos = async () => {
+
+
+
+productServer.get("/products", async (req, res) => {
     try {
+        const consulta = await manager.getProducts();
+        let limit = Number.parseInt(req.query.limit)
+
+        if (limit) {
+         
+            const resultado = consulta.slice(0, limit);
+            res.send(resultado);
+        } else {
+      
+            res.send(consulta);
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+productServer.get("/products/:pid", async (req, res) => {
+    try {
+        let id = req.params.pid
+        console.log(id)
+        const consultaId = await manager.getProductById(Number.parseInt(id));
+        if (!consultaId) {
+            console.log("ej");
+            return res.send({ error: "El producto con ese id no se encuentra en el archivo" });
+        } else {
+            res.send(consultaId);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+productServer.listen(8080, () => {
+    try {
+        console.log("Servidor arriba en el puerto 8080");
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
-
-
-
-        productServer.get("/products", async (req, res) => {
-
-            const consulta = await manager.getProducts();
-            let limite = req.query.limit
-
-            if (Number.parseInt(limite) === 5) {
-                console.log("a")
-                const resultado = consulta.slice(0, Number.parseInt(limite));
-                res.send(resultado);
-            } else {
-
-                res.send(consulta);
-            }
-
-
-        });
-
-        productServer.get("/products/:pid", async (req, res) => {
-            let id = req.params.pid
-            console.log(id)
-            const consultaId = await manager.getProductById(Number.parseInt(id));
-            if (!consultaId) {
-                console.log("ej");
-                return res.send({ error: "El producto con ese id no se encuentra en el archivo" });
-            } else {
-
-                res.send(consultaId);
-            }
-        });
-
-        productServer.listen(8080, () => {
-            console.log("Servidor arriba en el puerto 8080");
-        });
 
         // let product1 = await manager.addProduct("PR01", "Mouse Inalambrico Logitech", "Negro con led roja", 200, "mouse.jpg", 50)
         // console.log(product1);
@@ -55,10 +61,4 @@ const operacionesProductos = async () => {
         //  console.log(productAct);
         // let deleteproduct1 = await manager.deleteProducts(1);
         // console.log(deleteproduct1)
-    } catch (error) {
-        console.log(error);
-    }
 
-
-}
-operacionesProductos();
