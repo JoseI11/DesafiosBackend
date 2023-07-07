947974b6db97523bea3999746a2792b7e7b3a256
nacho <jose-imhoff@hotmail.com>
2023-07-06 21:49:03 -0300

    Refactorizacion app.js

--- desafios/src/app.js
+++ desafios/src/app.js
@@ -1,27 +1,16 @@
 import express from "express";
 import handlebars from 'express-handlebars'
-import __dirname from "./utils.js";
+import __dirname from "./dirname.js";
 import socket from './socket.js'
-import loggerRouter from "./routes/loggertest.router.js"
 import session from "express-session";
 import MongoStore from "connect-mongo";
-import productsRouter from './routes/products.router.js';
-import mailRouter from './routes/mail.router.js'
-import cartrouter from './routes/cart.router.js'
-import viewrouter from './routes/views.router.js'
 import database from "./db.js";
 import config from "./config.js";
-import mockRouter from "./routes/mocking.router.js";
-import sessionsRouter from "./routes/sessions.router.js"
-import usersRouter from "./routes/users.router.js"
-import smsRouter from "./routes/sms.router.js"
 import { winstonLogger } from "./utils/logger.js";
-
+import routesFunction from "./routes/app.router.js";
 import passport from "passport";
 import initializePassport from "./auth/passport.js";
 import cookieParser from "cookie-parser"
-// import passport from "passport";
-// import initializePassport from "../middlewares/passport.js";
 
 //Initialization
 const productServer = express();
@@ -46,6 +35,7 @@ productServer.use(
   })
 );
 initializePassport()
+routesFunction(productServer)
 productServer.use(passport.initialize())
 productServer.use(passport.session())
 
@@ -53,8 +43,6 @@ productServer.use(passport.session())
 productServer.engine("handlebars", handlebars.engine());
 productServer.set("views", `${__dirname}/views`);
 productServer.set("view engine", "handlebars");
-
-
 
 const httpServer = productServer.listen(8080, (req, res) => {
   try {
@@ -69,17 +57,6 @@ const httpServer = productServer.listen(8080, (req, res) => {
 });
 database.connect();
 
-productServer.get("/mockingproducts", mockRouter)
-productServer.use("/recovery", mailRouter)
-productServer.get("/sms", smsRouter)
-//productServer.use("/chat",chatRouter);
-productServer.use("/api/sessions", sessionsRouter);
-//productServer.get("/loggerTest",loggerRouter)
-productServer.use("/api/products", productsRouter);
-productServer.get("/loggerTest", loggerRouter);
-productServer.use("/api/carts", cartrouter);
-productServer.use("/api/users",usersRouter)
-productServer.use("/", viewrouter);
 socket.connect(httpServer)
 
 
